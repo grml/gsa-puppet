871a08a2caacf0c253a291b85c2dfe8a345cd5fc
Alexander Wirt <formorer@debian.org>
2011-08-08 13:52:20 +0200

    Use service for restart

--- modules/grml/manifests/init.pp
+++ modules/grml/manifests/init.pp
@@ -44,7 +44,7 @@ class grml {
         "/etc/xinetd.d/check_mk":
             require => Package["xinetd"],
             source => "puppet:///modules/grml/etc/xinetd.d/check_mk",
-            notify  => Exec["/etc/init.d/xinetd restart"];
+            notify  => Exec["service xinetd restart"];
 
 		"/etc/apt/sources.list.d/grml.list":
 			content => template("grml/etc/apt/sources.list.d/grml.list.erb"),
