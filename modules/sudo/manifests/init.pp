aca647d6f5914e4d7cb8048d38f0ba35d026707b
Alexander Wirt <formorer@debian.org>
2011-10-31 21:59:38 +0100

    Add backuppc sudo entry

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -11,6 +11,11 @@ class sudo {
 	    "set spec[user = '%wheel']/host_group/command[2] '/usr/bin/apt-get'",
 	    "set spec[user = '%wheel']/host_group/command[2]/tag NOPASSWD",
 	    "set spec[user = '%wheel']/host_group/command[3] '/usr/bin/aptitude'",
+        "set spec[user = 'backuppc']/user backuppc",
+        "set spec[user = 'backuppc']/host_group/host ALL",
+        "set spec[user = 'backuppc']/host_group/command[1] 'ALL'",
+        "set spec[user = 'backuppc']/host_group/command[1]/tag NOPASSWD",
+        "set spec[user = 'backuppc']/host_group/command[1] '/usr/bin/rsync'",
 	    "set /Defaults/env_keep/var[01] MAINTAINER"
 	    ],
 	    require => [ Package["sudo"], Package["augeas-lenses"] ]
