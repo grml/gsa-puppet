64dbf6f4582bac2221f0fed4a4ea87d988a78019
Alexander Wirt <formorer@debian.org>
2012-01-17 17:42:24 +0100

    Domain is not needed

--- modules/ferm/manifests/rsync.pp
+++ modules/ferm/manifests/rsync.pp
@@ -1,6 +1,5 @@
 class ferm::rsync {
     @ferm::rule { "dsa-rsync":
-        domain          => "(ip ip6)",
         description     => "Allow rsync access",
         rule            => "&SERVICE(tcp, 873)"
     }
