ef70699966dd5ccdb19b5ec9f33c60c784da0511
Ulrich Dangel <mru@grml.org>
2012-01-30 13:10:33 +0100

    Fix indentation of serial file

--- modules/serial/manifests/init.pp
+++ modules/serial/manifests/init.pp
@@ -1,33 +1,31 @@
 class serial {
-
-	exec { "update-grub":
-		command => "update-grub",
-		path        => "/etc/init.d:/usr/bin:/usr/sbin:/bin:/sbin",
-		refreshonly => true
-	}
+  exec { "update-grub":
+    command => "update-grub",
+    path        => "/etc/init.d:/usr/bin:/usr/sbin:/bin:/sbin",
+    refreshonly => true
+  }
+  exec { "init-change":
+    command => "telinit q",
+    path        => "/etc/init.d:/usr/bin:/usr/sbin:/bin:/sbin",
+    refreshonly => true
+  }
   augeas{"serial-grub":
     context => "/files/etc/default/grub",
-	  changes => [
+    changes => [
 		"set GRUB_CMDLINE_LINUX_DEFAULT '\"console=ttyS0,115200 console=tty0 quiet\"'"
 		],
-		notify  => Exec["update-grub"];
-	}
+    notify  => Exec["update-grub"];
+  }
 
-	exec { "init-change":
-		command => "telinit q",
-		path        => "/etc/init.d:/usr/bin:/usr/sbin:/bin:/sbin",
-		refreshonly => true
-	}
-	augeas{"serial-inittab":
-		context => "/files/etc/inittab/",
-		changes => [
+
+  augeas{"serial-inittab":
+    context => "/files/etc/inittab/",
+    changes => [
 		'set ser/runlevels "1223"',
 		'set ser/action  "respawn"',
 		'set ser/process  "/sbin/getty -L ttyS0 115200 vt100"'
 		],
-		onlyif => "match *[process =~ regexp('.*ttyS0.*')] size == 0",
+    onlyif => "match *[process =~ regexp('.*ttyS0.*')] size == 0",
 		notify  => Exec["init-change"];
-	}	
-
-
+  }
 }
