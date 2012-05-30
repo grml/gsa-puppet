c4cf6fc3ef550700f57fd1b8b2dbd5d75411e6e5
Alexander Wirt <formorer@debian.org>
2012-05-30 21:19:52 +0200

    Add LPK to ssh module

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -10,4 +10,40 @@ class ssh {
 	    require => File["/home/$username/.ssh/authorized_keys"]
 	  }
 	}
+
+    package { 'openssh-server':
+        ensure => latest
+    }
+
+    file { '/etc/ssh/sshd_config':
+        owner => root,
+              group => root,
+              mode => '0644',
+              notify => Service['ssh'],
+              require => Package['openssh-server'],
+    }
+
+    service { 'ssh':
+        ensure => running,
+        enable => true,
+        hasrestart => true,
+        hasstatus => true,
+        require => [
+            File['/etc/ssh/sshd_config'],
+            Package['openssh-server']
+        ],
+    }
+
+    augeas { "sshd_config":
+        context => "/files/etc/ssh/sshd_config",
+                changes => [
+                    "set UseLPK yes",
+                    "set LpkServers ldap://10.0.3.1/",
+                    "set LpkUserDN ou=People,dc=grml,dc=org",
+                    "set LpkGroupDN ou=Group,dc=grml,dc=org",
+                    "set LpkSearchTimelimit 3",
+                    "set LpkBindTimelimit 3",
+                    "set LpkForceTLS no"
+                ],
+    }
 }
