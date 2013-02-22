abe46d48dacf624915854a20384359e21020a91a
Alexander Wirt <formorer@grml.org>
2013-02-22 11:57:24 +0100

    Add ncurses-term to standard packages

--- modules/grml/manifests/init.pp
+++ modules/grml/manifests/init.pp
@@ -14,6 +14,7 @@ class grml {
               puppet,
               apt-dater-host,
               imvirt,
+	      ncurses-term,
               grml-etc-core,
               etckeeper,
               check-mk-agent,
