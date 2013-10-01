9b5fc87dfd9ccad89d6b2c10cb9c42541bc66d28
Alexander Wirt <formorer@grml.org>
2013-10-01 15:34:21 +0200

    Add puppetlabs repo

--- dist/grml/manifests/init.pp
+++ dist/grml/manifests/init.pp
@@ -25,6 +25,14 @@ class grml {
               ]
     : ensure => installed }
 
+
+  apt::source { 'puppetlabs':
+      location   => 'http://apt.puppetlabs.com',
+      repos      => 'main',
+      key        => '4BD6EC30',
+      key_server => 'pgp.mit.edu',
+  }
+
   file    {
     "/etc/cron.d/puppet":
       owner => root,
