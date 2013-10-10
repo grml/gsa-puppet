b3168deecf20923176064e81ce9029950e19821c
Alexander Wirt <formorer@grml.org>
2013-10-10 13:13:02 +0200

    Add submission and smtps ports to Ferm::smtp

--- dist/ferm/manifests/smtp.pp
+++ dist/ferm/manifests/smtp.pp
@@ -3,5 +3,13 @@ class ferm::smtp {
         description     => "Allow smtp access",
         rule            => "&SERVICE(tcp, smtp)"
     }
+    @ferm::rule { "submission":
+        description     => "Allow submission access",
+        rule            => "&SERVICE(tcp, submission)"
+    }
+    @ferm::rule { "smtps":
+        description     => "Allow smtps access",
+        rule            => "&SERVICE(tcp, smtps)"
+    }
 }
 
